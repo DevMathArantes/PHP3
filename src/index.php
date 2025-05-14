f5f6782ab4349618c05de1fe0e5484c659b50ee8
math.arantes <matheusoficial1411@gmail.com>
2025-05-14 14:18:50 -0300

    curso 52%

--- src/index.php
+++ src/index.php
@@ -1,16 +1,26 @@
 <?php 
 
-    require __DIR__ . "/functions.php";
     require __DIR__ . "/model/Filme.php";
+    require __DIR__ . "/model/Genero.php";
 
-    $filme1 = novoFilme("Star Wars: Ameaça Fantasma", 1999, "Ação", 6.5);
+    //Para atribuir valor a um enum, use X::Y para atribuir Y ao enum X
+    $filme1 = new Filme("Star Wars: Ameaça Fantasma", 1999, Genero::Ficcao, 6.5);
     $filme1->receberNota(4.5);
     $filme1->receberNota(9.5);
     $filme1->receberNota(7.2);
     $filme1->receberNota(6.8);
-    echo "\n" . $filme1->nome . " possui uma avaliação média de " . $filme1->calcularMedia() . "\n";
+    echo $filme1->nome . " recebeu a avaliação média " . $filme1->calcularMedia() . " de 10.\n"; 
 
-    $filme2 = novoFilme("Star Wars: Ataque dos Clones", 2002, "Ação", 6.6);
-    $filme3 = novoFilme("Star Wars: A Vingança dos Sith", 2005, "Ação", 7.6);
+    $filme2 = new Filme("Star Wars: Ataque dos Clones", 2002, Genero::Ficcao, 6.6);
+    $filme2->receberNota(5.0);
+    $filme2->receberNota(10.0);
+    $filme2->receberNota(8.3);
+    $filme2->receberNota(6.7);
+    echo $filme2->nome . " recebeu a avaliação média " . $filme2->calcularMedia() . " de 10.\n"; 
 
-
+    $filme3 = new Filme("Star Wars: A Vingança dos Sith", 2005, Genero::Ficcao, 7.6);
+    $filme3->receberNota(10);
+    $filme3->receberNota(10);
+    $filme3->receberNota(10);
+    $filme3->receberNota(10);
+    echo $filme3->nome . " recebeu a avaliação média " . $filme3->calcularMedia() . " de 10.\n"; 
--- src/model/Filme.php
+++ src/model/Filme.php
@@ -3,30 +3,58 @@
     /*
         O comando:
         class X {
-            public Y1 Z1
-            public Y2 Z2
+            N1 Y1 Z1
+            N2 Y2 Z2
             ...
-            public Yn Zn 
+            Nn Yn Zn 
         }
-        Cria uma classe X com atributos Y publicos do tipo Z 
+        Cria uma classe X com atributos Z publicos do tipo Y e acesso N 
     */
     class Filme {
-        public string $nome;
-        public int $ano;
-        public string $genero;
-        
-        //A visibilidade private faz com que a variável só possa ser acessada no arquivo onde foi declarada
-        private array $notas= [];
 
-        function receberNota(float $nota): void{
+        /*
+            A visibilidade public faz com que a variável seja acessivel em outros arquivos
+            
+            A visibilidade private faz com que a variável só possa ser acessada no arquivo onde foi declarada
+        */
+
+        private array $notas;
+
+        /*
+            Método construtor:
+            public function __construct(private $N1, private $N2..., private $Nn){
+            }
+
+            Para uma classe Y com esse método construtor, os setters poderiam ser substituidos durante a própria 
+            criação do objeto com $O = new Y(Z1, Z2..., Zn); atribuindo os valores Z aos atributos N
+
+            A única função que pode criar atributos através dos parâmetros é o método __construct()
+        */
+        public function __construct(
+
+            /*
+                A propriedade readonly faz com que a variável só possa ser atribuída uma única (o valor dela não poderá 
+                ser mudado em hipótese alguma) vez e isso deve ocorrer especificamente na classe onde ela foi criada 
+                (nem mesmo em classes filhas)
+            */
+            public readonly string $nome, 
+            public readonly int $ano, 
+            public readonly Genero $genero){
+
+            //O comando $this->X referência o atributo X do objeto
+            $this->notas = [];
+        }
+
+        public function receberNota(float $nota): void{
 
             //Para utilizar um atributo X do objeto, utilize $this->X
             $this->notas[] = $nota;
-            echo $this->nome . " recebeu a nota: " . $nota . "\n";
         }
-        function calcularMedia() : float{
+
+        public function calcularMedia() : float{
             $notas = $this->notas;
             $totalNotas = array_sum($notas);
-            return $totalNotas/count($notas);
+            return  array_sum($notas)/count($notas);
         }
+
     }
